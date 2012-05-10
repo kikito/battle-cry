a347ba535f54d2dd357cf17f9ff2f9c202e30105
Enrique García Cota <kikito@gmail.com>
2012-05-10 23:10:47 +0200

    Print a better name for the enter key on keypress menu. fixes #1

--- src/game/states/keyboard_menu.lua
+++ src/game/states/keyboard_menu.lua
@@ -4,18 +4,27 @@ local MainMenu = require 'src.game.states.main_menu'
 
 local KeyboardMenu = Game:addState('KeyboardMenu', MainMenu)
 
+local keyNames = {
+  [' ']= 'space'
+}
+
+local function getKeyName(key)
+  return keyNames[key] or key
+end
+
 function KeyboardMenu:enteredState()
-  self:log('Entering Keyboard')
+  self:log('Entering KeyboardMenu')
 
   local menuOptions = {}
 
   local actions = { 'up', 'down', 'left', 'right', 'action' }
 
   for i=1,#actions do
-    local action = actions[i]
-    local key = self.config.keys[action]
+    local action  = actions[i]
+    local key     = self.config.keys[action]
+    local keyName = getKeyName(key)
     menuOptions[#menuOptions + 1] = {
-      ("%s (%s)"):format(action, key),
+      ("%s (%s)"):format(action, keyName),
       function()
         self.modifiedAction = action
         self:gotoState("ChoosingKey")
