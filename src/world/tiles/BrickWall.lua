8e82c1a6596a002a39e37bb10aaa83f7ea7e83a3
Enrique García Cota <kikito@gmail.com>
2012-07-15 12:38:56 +0200

    Wall -> BrickWall

--- src/world/tiles/BrickWall.lua
+++ src/world/tiles/BrickWall.lua
@@ -1,9 +1,9 @@
 local Tile = require 'src.world.tiles.Tile'
 local Stone = require 'src.world.tiles.Stone'
 
-local Wall = class(..., Tile)
+local BrickWall = class(..., Tile)
 
-function Wall:initialize(x,y)
+function BrickWall:initialize(x,y)
   Tile.initialize(self,x,y,1,2)
   self.z = 1
   self.solid = true
@@ -11,4 +11,4 @@ function Wall:initialize(x,y)
   Stone:new(x,y)
 end
 
-return Wall
+return BrickWall
